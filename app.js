d979aaf6836d0ca39fb52fc4a65e793ef83adce4
Rick Burgess <git@rickburgess.me>
2012-05-15 09:23:13 +0100

    jade+less+twitter bootstrap working

--- app.js
+++ app.js
@@ -1,39 +1,49 @@
-
 /**
  * Module dependencies.
  */
 
-var express = require('express')
-  , routes = require('./routes');
+var express = require('express'), routes = require('./routes');
+
+var lessMiddleware = require('less-middleware');
+
+var pub = __dirname + '/public';
 
 var app = module.exports = express.createServer();
 
 // Configuration
 
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(express.cookieParser());
-  app.use(express.session({ secret: 'your secret here' }));
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
+app.configure(function() {
+	app.set('views', __dirname + '/views');
+	app.set('view engine', 'jade');
+	app.use(express.bodyParser());
+	app.use(express.methodOverride());
+	app.use(express.cookieParser());
+	app.use(express.session({
+		secret : 'alsdkjaslkdjaslkdjaslkd'
+	}));
+	app.use(lessMiddleware({
+		src : pub,
+		compress : true,
+		debug: false
+	}));
+	app.use(app.router);
+	app.use(express.static(pub));
 });
 
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+app.configure('development', function() {
+	app.use(express.errorHandler({
+		dumpExceptions : true,
+		showStack : true
+	}));
 });
 
-app.configure('production', function(){
-  app.use(express.errorHandler());
+app.configure('production', function() {
+	app.use(express.errorHandler());
 });
-
 // Routes
 
 app.get('/', routes.index);
 
-var port = process.env.PORT || 3000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+app.listen(3000, function() {
+	console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
