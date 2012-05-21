df36daa89993e7961441459183db615acf90ab04
Rick Burgess <git@rickburgess.me>
2012-05-21 20:16:16 +0100

    not sure various changes I think

--- app.js
+++ app.js
@@ -2,8 +2,8 @@
  * Module dependencies.
  */
 
-var express = require('express'), routes = require('./routes');
-
+var express = require('express'), routes = require('./routes'), fb = require('facebook-js'), fs = require('fs');
+var users = require('./routes/users');
 var lessMiddleware = require('less-middleware');
 
 var pub = __dirname + '/public';
@@ -24,26 +24,121 @@ app.configure(function() {
 	app.use(lessMiddleware({
 		src : pub,
 		compress : true,
-		debug: false
+		debug : false
 	}));
 	app.use(app.router);
 	app.use(express.static(pub));
 });
-
+var appID
 app.configure('development', function() {
 	app.use(express.errorHandler({
 		dumpExceptions : true,
 		showStack : true
 	}));
+	fbAppID = '364683150255491';
+	fbSecret = '8efbb7529522c1d71549ddc7eff2a000';
 });
 
 app.configure('production', function() {
 	app.use(express.errorHandler());
+	fbAppID = '364683150255491';
+	fbSecret = '8efbb7529522c1d71549ddc7eff2a000';
 });
-// Routes
 
+//Sort out mongo
+var mongo = {
+	"hostname" : "ds033477.mongolab.com",
+	"port" : 33477,
+	"username" : "heroku_app4649681",
+	"password" : "4649681",
+	"db" : "heroku_app4649681"
+}
+
+var generate_mongo_url = function(obj) {
+	obj.hostname = (obj.hostname || 'localhost');
+	obj.port = (obj.port || 27017);
+	obj.db = (obj.db || 'test');
+
+	if(obj.username && obj.password) {
+		return "mongodb://" + obj.username + ":" + obj.password + "@" + obj.hostname + ":" + obj.port + "/" + obj.db;
+	} else {
+		return "mongodb://" + obj.hostname + ":" + obj.port + "/" + obj.db;
+	}
+}
+mongourl = generate_mongo_url(mongo);
+// Routes
 app.get('/', routes.index);
 
+app.get('/fb_authorize', function(req, res) {
+	res.redirect(fb.getAuthorizeUrl({
+		client_id : fbAppID,
+		redirect_uri : 'http://localhost:5000/fb_thanks',
+		scope : ''//perms
+	}));
+});
+
+app.get('/fb_thanks', function(req, res) {
+	fb.getAccessToken(fbAppID, fbSecret, req.param('code'), 'http://localhost:5000/fb_thanks', function(error, access_token, refresh_token) {
+		res.render('client', {
+			access_token : access_token,
+			refresh_token : refresh_token,
+			title : 'Thanks!'
+		});
+	});
+});
+
+app.get('/user/save', users.save);
+
+//Compressed JS
+app.get('/scripts.js', function(req, res) {
+	get_js(false, function(js) {
+		console.log(js);
+		res.contentType('text/js');
+		res.end(js);
+	});
+});
+var get_js = function(compress, callback) {
+	//load reqs
+	var path = require('path');
+	var base = pub + '/app/javascripts';
+	var minpath = base + '/scripts.min.js';
+
+	if(compress) {
+		fs.unlinkSync(minpath);
+	}
+
+	if(path.existsSync(minpath)) {
+		var js = '';
+		js = fs.readFileSync(minpath);
+		if( typeof callback == 'function') {
+			callback(js);
+		}
+	} else {
+		var js = '';
+		fs.readdir(base, function(err, files) {
+			files.forEach(function(file) {
+				if(file != 'scripts.min.js') {
+					js += fs.readFileSync(base + '/' + file);
+				}
+			});
+			if(compress) {
+				var jsp = require("uglify-js").parser;
+				var pro = require("uglify-js").uglify;
+				console.log('compressing');
+				js = jsp.parse(js);
+				js = pro.ast_mangle(js);
+				js = pro.ast_squeeze(js);
+				js = pro.gen_code(js);
+				fs.writeFile(minpath, js);
+			}
+			if( typeof callback == 'function') {
+				callback(js);
+			}
+		});
+	}
+
+}
+get_js(false);
 //Start Listening
 var port = process.env.PORT || 3000;
 app.listen(port, function() {
---
+++ public/app/javascripts/app.js
@@ -0,0 +1,96 @@
+var uid = 0
+var accessToken = 0;
+var user;
+var session = new Array();
+window.fbAsyncInit = function() {
+	FB.init({
+		appId : '364683150255491', // App ID
+		channelUrl : '//localhost:5000/channel.html', // Channel File
+		status : true, // check login status
+		cookie : true, // enable cookies to allow the server to access the session
+		xfbml : true  // parse XFBML
+	});
+
+	FB.getLoginStatus(function(response) {
+		if(response.status === 'connected') {
+			uid = response.authResponse.userID;
+			accessToken = response.authResponse.accessToken;
+			console.log('calling /me');
+			FB.api('/me', function(response) {
+				user = response;
+				session.user = user;
+				update_login_info();
+			});
+		} else if(response.status === 'not_authorized') {
+			login_dialog();
+		} else {
+			login_dialog();
+		}
+	});
+	// Additional initialization code here
+};
+// Load the SDK Asynchronously
+( function(d) {
+	var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+	if(d.getElementById(id)) {
+		return;
+	}
+	js = d.createElement('script');
+	js.id = id;
+	js.async = true;
+	js.src = "//connect.facebook.net/en_US/all.js";
+	ref.parentNode.insertBefore(js, ref);
+}(document));
+
+$(document).ready(function() {
+	var myOptions = {
+		center : new google.maps.LatLng(51.507222, -0.1275),
+		zoom : 14,
+		mapTypeId : google.maps.MapTypeId.ROADMAP
+	};
+	$('body').prepend('<div id="map_canvas"></div>');
+	$('#map_canvas').height($(window).height());
+	map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+	navigator.geolocation.getCurrentPosition(GetLocation);
+});
+var login_dialog = function() {
+	var title = '<i class="icon-lock icon-white"></i> Login?';
+	var content = 'Hi!, would you like to login with Facebook?';
+	var login_dialog = $('<div></div>').html(content).dialog({
+		autoOpen : false,
+		title : title,
+		draggable : false,
+		resizable : false
+	});
+
+	login_dialog.dialog("option", "buttons", [{
+		text : "No!",
+		click : function() {
+			$(this).dialog("close");
+		}
+	}, {
+		text : "Yes!",
+		click : function() {
+			window.location = '/fb_authorize';
+			$(this).dialog("close");
+		}
+	}]);
+	login_dialog.dialog('open');
+}
+var update_login_info = function() {
+	if(user.first_name) {
+		$('#login').attr('href', '');
+		$('#login').html('Hi ' + user.first_name + '!');
+	}
+}
+
+function GetLocation(location, callback) {
+    session.location = location;
+    session.location.gmap = new google.maps.LatLng(location.coords.latitude, location.coords.longitude);
+    map.panTo(session.location.gmap);
+    var marker = new google.maps.Marker({
+        position: session.location.gmap, 
+        map: map,
+        title:"Hello World!"
+    }); 
+}
